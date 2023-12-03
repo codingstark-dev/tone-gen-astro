9c6cfd79431181e077b43b539297211995b9abb2
Himanshu Maurya <mhimanshu.338@gmail.com>
2023-12-03 13:06:57 +0530

    added language and fandq

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -2,9 +2,10 @@ import type { AstroI18nextConfig } from "astro-i18next";
 
 const config: AstroI18nextConfig = {
   defaultLocale: "en",
-  locales: ["en", "fr"],
+  locales: ["en", "fr","es","ru","id"],
   i18nextServer: {
     debug: true,
+    
   },
 };
 
