cfb35f6072f44e3a624b929ee755ef43c0f40cf8
Himanshu Maurya <mhimanshu.338@gmail.com>
2024-01-28 20:09:15 +0530

    Added Noise component for generating different types of noise

---
+++ src/components/Noise.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import * as Tone from 'tone';
+
+const Noise = () => {
+    const [noise, setNoise] = React.useState<Tone.Noise | null>(null);
+    const [noiseType, setNoiseType] = React.useState<'white' | 'pink' | 'brown' | "none">('white'); // ['white', 'pink', 'brown'
+
+    const generateWhiteNoise = () => {
+        if (noise) {
+            noise.stop();
+        }
+        const newNoise = new Tone.Noise('white').toDestination();
+        newNoise.start();
+        setNoiseType('white');
+        setNoise(newNoise);
+    };
+
+    const generatePinkNoise = () => {
+        if (noise) {
+            noise.stop();
+        }
+
+        const newNoise = new Tone.Noise('pink').toDestination();
+        setNoiseType('pink');
+        newNoise.start();
+        setNoise(newNoise);
+    };
+
+    const generateBrownNoise = () => {
+        if (noise) {
+            noise.stop();
+        }
+        const newNoise = new Tone.Noise('brown').toDestination();
+        setNoiseType('brown');
+        newNoise.start();
+        setNoise(newNoise);
+    };
+
+    const stopNoise = () => {
+        if (noise) {
+            noise.stop();
+            setNoiseType('none');
+            setNoise(null);
+        }
+    };
+
+    return (
+        <div className='grid grid-cols-1 gap-4 items-center justify-center text-center '>
+            <div>
+                <button className='btn-square btn btn-wide font-bold' onClick={generateWhiteNoise}>White Noise {
+                    noiseType === 'white' ? <span className='text-green-500 font-extrabold'>ON</span> : null
+                }</button></div>
+            <div>
+                <button className='btn-square btn btn-wide bg-pink-500 text-white  font-bold' onClick={generatePinkNoise}>Pink Noise
+                    {noiseType === 'pink' ? <span className='text-green-500 font-extrabold'>ON</span> : null}
+                </button></div>
+            <div><button className='btn-square btn btn-wide font-bold bg-red-950 text-white' onClick={generateBrownNoise}>Brown Noise
+                {noiseType === 'brown' ? <span className='text-green-500 font-extrabold'>ON</span> : null}
+            </button></div>
+            <div> <button className='btn-square btn btn-wide font-bold bg-red-500 text-white' onClick={stopNoise}>Stop Noise</button></div>
+        </div>
+    );
+};
+
+export default Noise;
